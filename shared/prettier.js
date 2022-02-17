5971226d2911a27f9060923e159e64fd1ca8831f
Bernardo Vieira <bernardo@impactmarket.com>
2022-02-17 12:04:29 +0000

    prettier cleanup

--- shared/prettier.js
+++ shared/prettier.js
@@ -7,8 +7,11 @@ module.exports = {
             {
                 arrowParens: 'avoid',
                 bracketSameLine: false,
+                endOfLine: 'lf',
                 printWidth: 120,
+                semi: true,
                 singleQuote: true,
+                tabWidth: 4,
                 trailingComma: 'none'
             }
         ]
